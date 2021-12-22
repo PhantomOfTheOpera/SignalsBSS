36eda06555e0efe567a7d80105172cee0b7c7455
PhantomOfTheOpera <oleksa.hryniv@gmail.com>
2021-12-22 19:12:37 +0200

    updated environment

--- modelling/__init__.py
+++ modelling/__init__.py
@@ -2,7 +2,6 @@
 
 from modelling.environment import Environment
 from modelling.microphone import Mic
-#from modelling.point import SoundPoint
+from modelling.point import SoundPoint
 from modelling.source import Source
-#from modelling.wall import Wall
-#from modelling.player import Player
+from modelling.player import Player
